e8eea2d4574f56c214e6fcf92691939b7e4f989f
mimura <musyu36@gmail.com>
2019-10-11 16:05:20 +0900

    update files

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 console.log(process.argv);
 console.log(process.argv[2]);
+
+var request = require('request');
+
+var options = {
+    url: process.argv[2],
+    method: 'GET'
+}
+
+request(options, function (error, response, body) {
+    console.log(body);
+})
