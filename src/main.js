e3f0d99a7169e038275178d25b3fe8a5e49db4ee
mimura <musyu36@gmail.com>
2019-10-12 17:39:42 +0900

    add -d option

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ function capitalizeFirstLetter(str) {
         var str_1 = str.slice(0, str.indexOf('-'));
         var str_2 = str.slice(str.indexOf('-') + 1);
         return str_1.charAt(0).toUpperCase() + str_1.slice(1) + '-' + str_2.charAt(0).toUpperCase() + str_2.slice(1);
-    }   
+    }
     else {
         return str.charAt(0).toUpperCase() + str.slice(1);    
     }
@@ -46,6 +46,13 @@ argv.option([
         type: 'string',
         description: 'HTTPメソッドの指定',
         example: "'curl_c -X POST url' or 'curl_c --request POST url'"
+    },
+    {
+        name: 'data',
+        short: 'd',
+        type: 'string',
+        description: 'HTTPメソッドの指定',
+        example: "'curl_c -X POST url' or 'curl_c --request POST url'"
     }
 ]);
 
@@ -65,19 +72,36 @@ var options = {
     method: method_set
 }
 
+// POSTリクエストとしてフォームを送信
+if (arg['options']['data']) {
+    // optionsに追加する連想配列
+    var data_set = {};
+
+    // キーと値のセットを&で抽出
+    var data_sliced = arg['options']['data'].split(/\&/);
+
+    // キーと値を=で抽出
+    data_sliced.forEach(function (key_value) {
+        data_set[key_value.slice(0, key_value.indexOf('='))] = key_value.slice(key_value.indexOf('=') + 1);
+    });
+
+    // optionsに追加
+    options['form'] = data_set;
+}
+
 request(options, function (error, res, body) {
     // 詳細ログの出力
     if (arg['options']['verbose']) {
-        console.log("* Conencted to" + res['request']['uri']['host'], "port", res['request']['uri']['port']);
+        console.log("* Connected to " + res['request']['uri']['host'], "port", res['request']['uri']['port']);
         
-        header_sp = res['req']['_header'].split(/\n/);
+        var header_sp = res['req']['_header'].split(/\n/);
         for (var i = 0; i < 2; i++){
             console.log('> ' + capitalizeFirstLetter(header_sp[i]));
         }
         console.log('> ');
 
-        for (var key in res.headers) {
-            console.log('< ' + capitalizeFirstLetter(key) + ': ' + res.headers[key]);
+        for (var key in res['headers']) {
+            console.log('< ' + capitalizeFirstLetter(key) + ': ' + res['headers'][key]);
         }
         console.log('< ');
     }
