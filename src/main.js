2e31788851d45cda2d386d491514fb3266d54a85
mimura <musyu36@gmail.com>
2019-10-14 16:58:31 +0900

    update file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var argv = require('argv');
 var fs = require('fs');
+var nslookup = require('nslookup');
 
 // ファイル書き込み
 function writeFile(path, data) {
@@ -22,8 +23,6 @@ function capitalizeFirstLetter(str) {
         return str.charAt(0).toUpperCase() + str.slice(1);    
     }
 }
-
-
 
 argv.option([
     {
@@ -72,46 +71,79 @@ var options = {
     method: method_set
 }
 
-// POSTリクエストとしてフォームを送信
-if (arg['options']['data']) {
-    // optionsに追加する連想配列
-    var data_set = {};
-
-    // キーと値のセットを&で抽出
-    var data_sliced = arg['options']['data'].split(/\&/);
-
-    // キーと値を=で抽出
-    data_sliced.forEach(function (key_value) {
-        data_set[key_value.slice(0, key_value.indexOf('='))] = key_value.slice(key_value.indexOf('=') + 1);
+const wait = (sec) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, sec * 1000);
     });
+};
 
-    // optionsに追加
-    options['form'] = data_set;
+
+var IP;
+async function main() {
+    try {
+        //-vオプションが指定されていれば
+        if (arg['options']['verbose']) {
+            // IPアドレスの取得
+            nslookup(options['url'], function (err, addrs) {
+                IP = addrs;
+            });
+            await wait(0.3);
+        }
+
+        // POSTリクエストとしてフォームを送信
+        if (arg['options']['data']) {
+            // optionsに追加する連想配列
+            var data_set = {};
+
+            // キーと値のセットを&区切りで抽出
+            var data_sliced = arg['options']['data'].split(/\&/);
+
+            // キーと値を=で分割して抽出
+            data_sliced.forEach(function (key_value) {
+                data_set[key_value.slice(0, key_value.indexOf('='))] = key_value.slice(key_value.indexOf('=') + 1);
+            });
+
+            // optionsに追加
+            options['form'] = data_set;
+        }
+
+        request(options, function (error, res, body) {
+
+            // -vオプションが指定されていれば詳細ログを標準出力
+            if (arg['options']['verbose']) {
+                // IPアドレスを取得できていればドメイン名と共に表示
+                if (IP) {
+                    console.log("* Connected to " + res['request']['uri']['host'], "(", IP[0], ")", "port", res['request']['uri']['port']);
+                } else {
+                    console.log("* Connected to " + res['request']['uri']['host'], "port", res['request']['uri']['port']);
+                }
+
+                var header_sp = res['req']['_header'].split(/\n/);
+                for (var i = 0; i < 2; i++) {
+                    console.log('> ' + capitalizeFirstLetter(header_sp[i]));
+                }
+                console.log('> User-Agent: curl_c/1.0.0');
+                console.log('> ');
+
+                for (var key in res['headers']) {
+                    console.log('< ' + capitalizeFirstLetter(key) + ': ' + res['headers'][key]);
+                }
+                console.log('< ');
+            }
+
+            //　出力先のファイルが指定されていなければHTMLを標準出力
+            if (typeof arg['options']['output'] == 'undefined') {
+                console.log(body);
+            }
+            //　出力先のファイルが指定されていればHTMLをファイルに出力
+            else if (typeof arg['options']['output'] !== 'undefined') {
+                writeFile(arg['options']['output'], body);
+            }
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
 }
 
-request(options, function (error, res, body) {
-    // 詳細ログの出力
-    if (arg['options']['verbose']) {
-        console.log("* Connected to " + res['request']['uri']['host'], "port", res['request']['uri']['port']);
-        
-        var header_sp = res['req']['_header'].split(/\n/);
-        for (var i = 0; i < 2; i++){
-            console.log('> ' + capitalizeFirstLetter(header_sp[i]));
-        }
-        console.log('> ');
-
-        for (var key in res['headers']) {
-            console.log('< ' + capitalizeFirstLetter(key) + ': ' + res['headers'][key]);
-        }
-        console.log('< ');
-    }
-
-    //　出力先のファイルが指定されていなければ標準出力
-    if (typeof arg['options']['output'] == 'undefined') {
-        console.log(body);
-    }
-    //　出力先のファイルが指定されていればファイルに出力
-    else if (typeof arg['options']['output'] !== 'undefined') {
-        writeFile(arg['options']['output'], body);
-    }
-})
+main();
